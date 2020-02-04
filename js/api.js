2253bcd827c7e2f5d1f57e417b899b311e6b3c60
mariamisa <maryaam1998@hotmail.com>
2020-02-04 13:42:34 +0200

    create xhr function relates #3

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
 function xhr(url,callback){
     const xhr=new XMLHttpRequest();
     xhr.onreadystatechange=function(){
-        if(xhr.readyState==4 && xhr.status==200){
+        if(xhr.readyState===4 && xhr.status===200){
             let data=xhr.responseText;
             data=JSON.parse(data);
             callback(data);
