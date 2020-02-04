2547ad4724b38d8ae3b0f68b316a4fcee2a559c2
mariamisa <maryaam1998@hotmail.com>
2020-02-04 13:38:17 +0200

    create xhr function relates #3

---
+++ js/api.js
@@ -0,0 +1,14 @@
+function xhr(url,callback){
+    const xhr=new XMLHttpRequest();
+    xhr.onreadystatechange=function(){
+        if(xhr.readyState==4 && xhr.status==200){
+            let data=xhr.responseText;
+            data=JSON.parse(data);
+            callback(data);
+            
+        }
+    }
+    xhr.open('GET',url,true);
+    xhr.send();
+}
+
